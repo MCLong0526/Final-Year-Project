69699ad655f03460b830a64bf0e2d4ca47cd827c
Michael Chia Cheng Long <mcclong@sains.com.my>
2024-02-29 22:22:16 +0800

    Initial commit

---
+++ resources/js/layouts/components/NavItems.vue
@@ -0,0 +1,31 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue';
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue';
+</script>
+
+<template>
+  
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Apps & Pages',
+    }"
+  />
+ 
+
+  <VerticalNavLink
+    :item="{
+      title: 'Test',
+      icon: 'ri-test-tube-line',
+      to: '/test',
+    }"
+  />
+
+  <VerticalNavLink
+    :item="{
+      title: 'Tables',
+      icon: 'ri-table-alt-line',
+      to: '/tables',
+    }"
+  />
+
+</template>
