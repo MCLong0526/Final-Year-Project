1a4657ef1e722fe1bef013ce3aecebd5c3adbaa4
Michael Chia Cheng Long <mcclong@sains.com.my>
2024-06-29 12:24:10 +0800

    add no follower alert 2

--- public/build/assets/ssrBoot-kxzWoMZw.js
+++
@@ -1,1 +0,0 @@
-import{ad as t,K as e,d as s,aW as n}from"./main-u_KdFkSh.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
